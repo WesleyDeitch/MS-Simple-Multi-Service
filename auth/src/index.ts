bfd6c1e492ab9ba73a5fa7d5ad38f6e8111a56d8
Wesley Deitch <deitchwesley@gmail.com>
2021-02-09 12:48:58 -0800

    Trying to fix error(could not find fix)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,10 @@ app.get('/api/users/currentuser', (req, res) => {
   res.send('Hi there!');
 });
 
+app.get('/api', (req, res) => {
+  res.send('hey there');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!!!!!!!!');
 });

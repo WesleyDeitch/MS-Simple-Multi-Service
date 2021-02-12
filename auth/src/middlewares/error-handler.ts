b01e36158b1c8ce56e92c1083a27da7692014e7d
Wesley Deitch <deitchwesley@gmail.com>
2021-02-12 13:19:12 -0800

    Adding logic with custom error to error middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -8,17 +7,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((err) => {
-      return { message: err.msg, field: err.param };
-    });
-    return res.status(400).send({ errors: formattedErrors });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({
-      errors: [{ message: err.reason }],
-    });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({

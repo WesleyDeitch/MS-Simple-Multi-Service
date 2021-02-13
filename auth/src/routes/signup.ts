81b2229037dd15056c1faaf6a680b686cf215030
Wesley Deitch <deitchwesley@gmail.com>
2021-02-12 18:47:35 -0800

    Added in user creation and hashing password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -22,11 +23,16 @@ router.post(
     }
 
     const { email, password } = req.body;
+    const existingUser = await User.find({ email });
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    return res.status(201).send(user);
   }
 );
 

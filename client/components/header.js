d85f2d670d3f52987f4f59a7cc601a956fdb76de
Wesley Deitch <deitchwesley@gmail.com>
2021-02-20 11:34:42 -0800

    Created the header

---
+++ client/components/header.js
@@ -0,0 +1,14 @@
+export default ({ currentUser }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/">
+        <a className="navbar-brand">GitTix</a>
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">
+          {currentUser ? 'Sign out' : 'Sign in/up'}
+        </ul>
+      </div>
+    </nav>
+  );
+};

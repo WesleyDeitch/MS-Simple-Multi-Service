3082ba6a9ef58c26076bcb9e209362a6444bab59
Wesley Deitch <deitchwesley@gmail.com>
2021-02-21 10:38:27 -0800

    Tickets Setup

---
+++ tickets/app.ts
@@ -0,0 +1,24 @@
+import express, { ErrorRequestHandler } from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { NotFoundError, errorHandler } from '@wdtickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+// Routers
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler as ErrorRequestHandler);
+
+export { app };
---
+++ tickets/index.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('No jwt key defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to mongodb');
+  } catch (e) {
+    console.log(e);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
+start();

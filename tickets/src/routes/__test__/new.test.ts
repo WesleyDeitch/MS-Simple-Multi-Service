b9f032d352e8425c646a3ee91ac5f0a9ebf2a409
Wesley Deitch <deitchwesley@gmail.com>
2021-02-23 09:51:49 -0800

    Finished new.ts and test file

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -59,7 +60,8 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
-  // add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
 
   await request(app)
     .post('/api/tickets')
@@ -68,4 +70,8 @@ it('creates a ticket with valid inputs', async () => {
       price: 10,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
 });

18ddda452cf154a868b2c3719ad8943e4574ba90
Wesley Deitch <deitchwesley@gmail.com>
2021-02-23 10:15:14 -0800

    Create index router and test file

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
 import { NotFoundError, errorHandler, currentUser } from '@wdtickets/common';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,6 +20,7 @@ app.use(currentUser);
 // Routers
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();

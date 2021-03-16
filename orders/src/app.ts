8d87e96a4577251b3fd64e2a0ceff447999eb384
Wesley Deitch <deitchwesley@gmail.com>
2021-03-16 12:03:42 -0700

    Orders initial setup

---
+++ orders/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
+import { NotFoundError, errorHandler, currentUser } from '@wdtickets/common';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/udpate';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+// Routers
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

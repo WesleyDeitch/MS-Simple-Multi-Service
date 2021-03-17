b42446fdf65ed77174d828eead7af7dad8aae87e
Wesley Deitch <deitchwesley@gmail.com>
2021-03-16 18:02:19 -0700

    Added isReserved method onto ticket model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@wdtickets/common';
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  OrderStatus,
+  BadRequestError,
+} from '@wdtickets/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -10,6 +18,29 @@ router.post(
   [body('ticketId').not().isEmpty().withMessage('TicketId must be provided')],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from taht means the ticket *is* reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event that an order was created
+
     res.send({});
   }
 );

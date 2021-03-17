439b7fcc6d6c65066aa3dba4845fee6f11ac17ec
Wesley Deitch <deitchwesley@gmail.com>
2021-03-17 15:23:52 -0700

    ID adjustment

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -2,10 +2,21 @@ import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, TicketCreatedEvent } from '@wdtickets/common';
 import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
+import { transform } from 'typescript';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { title, price, id } = data;
+    const ticket = Ticket.build({
+      title,
+      price,
+      id,
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
 }

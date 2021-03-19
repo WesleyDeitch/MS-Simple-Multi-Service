90148e3ee832ce5309c4ea99aecc19869b5f911a
Wesley Deitch <deitchwesley@gmail.com>
2021-03-19 09:10:58 -0700

    Listeners for ticket in orders service

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -2,7 +2,6 @@ import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, TicketCreatedEvent } from '@wdtickets/common';
 import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
-import { transform } from 'typescript';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
